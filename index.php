08ae2de290d4158605a31fdc10f049c9c1183d8b
Ubuntu <ubuntu@ip-10-128-41-94.ap-southeast-1.compute.internal>
2012-03-20 15:29:00 +1100

    clean urls

--- index.php
+++ index.php
@@ -1,19 +1,44 @@
+<html>
+<head><title>Chris' Blog</title></head>
+<body>
+
 <?php
 
 // Simple array:
 
-$dir   = '/home/ubuntu/web/blog';
+$dir   = '/home/ubuntu/web/blog/articles';
 $array = scandir($dir);
 $count = count($array);
 
+$urlVariables = explode("/",$_SERVER['REQUEST_URI']);
 
-$article = $_GET["article"];
+//var_dump($urlVariables);
+
+
+$date = $urlVariables[2];
+$article = $urlVariables[3];
+
 if(!empty($article))
-	include($article . ".html");
+	include("articles/" . $date . "-" .$article . ".html");
+else
+	include("home.html");
 
 for ($i = 2; $i < $count; $i++) {
-	if ($array[$i] != 'index.php' && $array[$i] != 'robots.txt')
-		echo "<li><a href='https://ausbots.com.au/blog/$array[$i]'> {$array[$i]}</a> <br/>";
+	$article = substr($array[$i], 9, -5);
+	$dateStr = substr($array[$i], 0, 8);
+	
+	echo "<li>";
+	
+	$time = strtotime($dateStr);
+	echo date("d M, Y", $time);
+	//$date = new DateTime($dateStr);
+	//$dateFormat = echo date("Y/m/d",$dateStr);
+		
+	echo "<a href='https://ausbots.com.au/blog/$dateStr/$article'> $article</a> <br/>";
+	echo "</li>";
 }
 
 ?>
+
+</body>
+</html>
